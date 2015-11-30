1ceef71019da8a88bf646dd81cb3c558077511c7
gcjones <gcjones@ncsu.edu>
2015-11-30 10:54:57 -0500

    zzzzzzz

--- Rain/src/com/thecherno/rain/Game.java
+++ Rain/src/com/thecherno/rain/Game.java
@@ -126,7 +126,7 @@ public class Game extends Canvas implements Runnable {
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle(game.title);
+		game.frame.setTitle(Game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
